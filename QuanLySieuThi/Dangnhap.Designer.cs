52e1d1592535558b24ef9ff42a2617f584005fbb
LeAnhTruc <leanhtruc520@gmail.com>
2023-11-17 00:58:28 +0700

    first commit

---
+++ QuanLySieuThi/Dangnhap.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace QuanLySieuThi
+{
+    partial class Dangnhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dangnhap));
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txt_mk = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txt_tk = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txt_mk);
+            this.groupBox2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(73, 154);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Size = new System.Drawing.Size(471, 78);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Mật Khẩu";
+            // 
+            // txt_mk
+            // 
+            this.txt_mk.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_mk.Location = new System.Drawing.Point(84, 33);
+            this.txt_mk.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txt_mk.Multiline = true;
+            this.txt_mk.Name = "txt_mk";
+            this.txt_mk.PasswordChar = '*';
+            this.txt_mk.Size = new System.Drawing.Size(315, 35);
+            this.txt_mk.TabIndex = 0;
+            this.txt_mk.TextChanged += new System.EventHandler(this.txt_mk_TextChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txt_tk);
+            this.groupBox1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(72, 68);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(471, 78);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tài khoản";
+            // 
+            // txt_tk
+            // 
+            this.txt_tk.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_tk.Location = new System.Drawing.Point(84, 33);
+            this.txt_tk.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txt_tk.Multiline = true;
+            this.txt_tk.Name = "txt_tk";
+            this.txt_tk.Size = new System.Drawing.Size(315, 37);
+            this.txt_tk.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button2.Location = new System.Drawing.Point(344, 297);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 54);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Thoát";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button1.Location = new System.Drawing.Point(72, 297);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(211, 54);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "     Đăng Nhập";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Enter += new System.EventHandler(this.button1_Click);
+            // 
+            // Dangnhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(617, 364);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Dangnhap";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Đăng Nhập Tài Khoản";
+            this.Load += new System.EventHandler(this.Dangnhap_Load);
+            this.Enter += new System.EventHandler(this.button1_Click);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txt_mk;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txt_tk;
+    }
+}
---
+++ sqltl.sql
@@ -0,0 +1,72 @@
+﻿use master
+go
+if exists (select name from sysdatabases where name='quanlyst1')
+drop Database quanlyst1
+go
+CREATE DATABASE quanlyst1;
+GO
+
+USE quanlyst1;
+GO
+
+CREATE TABLE chitietdonhang (
+    maphieuxuat int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+    sanphamban nvarchar(500) NULL,
+    ngayban datetime NULL,
+    thanhtien int NULL,
+    nguoiban nvarchar(50) NULL
+);
+
+CREATE TABLE nhacungcap (
+    mancc int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+    tenncc nvarchar(50) NULL,
+    diachi nvarchar(50) NULL,
+    sdt nvarchar(50) NULL,
+    congno int NULL
+);
+
+CREATE TABLE sanpham (
+    masp int IDENTITY(1000,1) NOT NULL PRIMARY KEY,
+    tensp nvarchar(50) NULL,
+    mancc int NULL,
+    gianhap int NULL,
+    giaban int NULL,
+    solg int NULL,
+    hsd date NULL,
+    noisx nvarchar(50) NOT NULL,
+    donvitinh nvarchar(50) NULL,
+    nguoinhap nvarchar(50) NULL
+);
+
+CREATE TABLE taikhoan (
+    username nvarchar(50) NOT NULL PRIMARY KEY,
+    password nvarchar(50) NULL,
+    fullname nvarchar(50) NULL,
+    datecreate nvarchar(25) NULL,
+	isAdmin int NULL
+
+);
+
+INSERT INTO chitietdonhang (sanphamban, ngayban, thanhtien, nguoiban) VALUES (N'Tuong ot : 1, Dau an : 2, Bot ngot : 2', '2022-08-24 22:44:05', 175000, N'Nguyen Van Teo (Quản trị)');
+-- Thêm các dòng dữ liệu khác cho bảng chitietdonhang
+-- Vui lòng sửa lại ngày tháng và dữ liệu phù hợp
+
+INSERT INTO nhacungcap (tenncc, diachi, sdt, congno) VALUES (N'VinFast', N'Hà Nội', N'01655888622', 0);
+-- Thêm các dòng dữ liệu khác cho bảng nhacungcap
+
+INSERT INTO sanpham (tensp, mancc, gianhap, giaban, solg, hsd, noisx, donvitinh, nguoinhap) VALUES (N'Quat Máy', 1, 300000, 400000, 6, '2025-01-01', N'Hải Phòng', N'Chiếc', N'Nguyen Van Dung (Quản trị)');
+-- Thêm các dòng dữ liệu khác cho bảng sanpham
+-- Vui lòng sửa lại ngày tháng và dữ liệu phù hợp
+
+INSERT INTO taikhoan (username, password, fullname,datecreate,isAdmin) VALUES (N'admin', N'123', N'Nguyen Van a',N'2022-55-30 10:55:52 PM',0);
+-- Thêm các dòng dữ liệu khác cho bảng taikhoan
+
+SET IDENTITY_INSERT chitietdonhang OFF;
+SET IDENTITY_INSERT nhacungcap OFF;
+SET IDENTITY_INSERT sanpham OFF;
+
+--DROP TABLE taikhoan;
+--DROP TABLE sanpham;
+--DROP TABLE chitietdonhang;
+--DROP TABLE nhacungcap;
+select * from taikhoan
